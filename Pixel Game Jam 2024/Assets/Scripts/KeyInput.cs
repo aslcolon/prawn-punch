6c9dec55c651f88bfe902d94656b3a6b7cd7d918
aslcolon <155334067+aslcolon@users.noreply.github.com>
2024-05-17 23:19:13 -0700

    Clean up and comment code

--- Pixel Game Jam 2024/Assets/Scripts/KeyInput.cs
+++ Pixel Game Jam 2024/Assets/Scripts/KeyInput.cs
@@ -16,12 +16,13 @@ public class KeyInput : MonoBehaviour
     // Declare and initialize holdCount integers that will determine which key holder is active
     private int holdCountP1 = 0, holdCountP2;
 
+    // Declare bool for P1 and P2 for checking inputs
+    // isHit checks if input is correct while wrongInput checks if input is incorrect
     private bool isHitP1 = false, isHitP2 = false, wrongInputP1 = false, wrongInputP2 = false;
 
     // Start is called before the first frame update
     void Start()
     {
-
         // Initialize holdCount integer for P2
         holdCountP2 = holdName.Length / 2;
 
@@ -36,37 +37,43 @@ public class KeyInput : MonoBehaviour
     void Update()
     { 
         // Check P1 key input for ascending key holder
-        for(int i = 0; i < holdName.Length / 2; i++)
-        {
-            if(holdCountP1 == i)
+        for (int i = 0; i < holdName.Length / 2; i++)
+        { 
+            if (holdCountP1 == i)
             {
+                // Check input for corresponding key holder to the referenced key displayed 
                 checkInputP1(refScript[i], KeyCode.W, KeyCode.A, KeyCode.S, KeyCode.D);
 
-                if(isHitP1 == true)
+                // If input is correct
+                if (isHitP1 == true)
                 {
+                    // Disable SpriteRenderer to corresponding sprite and set isHit back to false
                     GameObject.Find("Prefab Spawn" + (i + 1).ToString()).GetComponent<SpriteRenderer>().enabled = false;
                     isHitP1 = false;
                 }
-
-                else if(wrongInputP1 == true)
+                // Else if input is incorrect
+                else if (wrongInputP1 == true)
                 {
-                    for(int j = 0; j < holdName.Length / 2; j++)
+                    // For all key sprites within sequence
+                    for (int j = 0; j < holdName.Length / 2; j++)
                     {
-                        if(GameObject.Find("Prefab Spawn" + (j + 1).ToString()).GetComponent<SpriteRenderer>().enabled == false)
+                        // If sprites are disabled then enable them
+                        if (GameObject.Find("Prefab Spawn" + (j + 1).ToString()).GetComponent<SpriteRenderer>().enabled == false)
                         {
                             GameObject.Find("Prefab Spawn" + (j + 1).ToString()).GetComponent<SpriteRenderer>().enabled = true;
                         }
                     }
                     
                 }
-                
                 break;
             }
         }
 
-        if(holdCountP1 == 4)
+        // Once the sequence is complete and P1 reaches last key holder
+        if (holdCountP1 == 4)
         {
-            holdCountP1 = 0;
+            holdCountP1 = 0; // Reset key holder number
+            // Destroy all key game objects within sequence 
             for(int i = 0; i < holdName.Length / 2; i++)
             {
                 Destroy(GameObject.Find("Prefab Spawn" + (i + 1).ToString()));
@@ -74,48 +81,52 @@ public class KeyInput : MonoBehaviour
         }
 
         // Check P2 key input for ascending key holder
-        for(int j = holdName.Length / 2; j < holdName.Length; j++)
+        for (int j = holdName.Length / 2; j < holdName.Length; j++)
         {
-            if(holdCountP2 == j)
+            if (holdCountP2 == j)
             {
+                // Check input for corresponding key holder to the referenced key displayed
                 checkInputP2(refScript[j], KeyCode.UpArrow, KeyCode.LeftArrow, KeyCode.DownArrow, KeyCode.RightArrow);
 
+                // If input is correct
                 if (isHitP2 == true)
                 {
+                    // Disable SpriteRenderer to corresponding sprite and set isHit back to false
                     GameObject.Find("Prefab Spawn" + (j + 1).ToString()).GetComponent<SpriteRenderer>().enabled = false;
                     isHitP2 = false;
                 }
-
+                // Else if input is incorrect
                 else if (wrongInputP2 == true)
                 {
+                    // For all key sprites within sequence
                     for (int k = holdName.Length / 2; k < refScript.Length; k++)
                     {
+                        // If sprites are disabled then enable them
                         if (GameObject.Find("Prefab Spawn" + (k + 1).ToString()).GetComponent<SpriteRenderer>().enabled == false)
                         {
                             GameObject.Find("Prefab Spawn" + (k + 1).ToString()).GetComponent<SpriteRenderer>().enabled = true;
                         }
                     }
-
                 }
                 break;
             }
         }
 
+        // Once the sequence is complete and P2 reaches last key holder
         if (holdCountP2 == 8)
         {
-            holdCountP2 = holdName.Length / 2;
+            holdCountP2 = holdName.Length / 2; // Reset key holder number
+            // Destroy all key game objects within sequence
             for (int i = holdName.Length / 2; i < refScript.Length; i++)
             {
                 Destroy(GameObject.Find("Prefab Spawn" + (i + 1).ToString()));
             }
         }
-
     }
 
     // Check if key input matches key displayed for P1
     void checkInputP1(KeyHolder refScriptNum, KeyCode key1, KeyCode key2, KeyCode key3, KeyCode key4)
     {
-
         // If keyDir integer declared from KeyHolder script 
         if (refScriptNum.keyDir == 1)
         {

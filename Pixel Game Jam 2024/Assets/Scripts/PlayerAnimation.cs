f7170be945abb71ccb201936f80a2aa3e7629fed
aslcolon <155334067+aslcolon@users.noreply.github.com>
2024-05-18 13:20:31 -0700

    Implement animation when player is hit

---
+++ Pixel Game Jam 2024/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator animator;
+
+    public KeyHolder refIsSpawn4, refIsSpawn8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        refIsSpawn4 = GameObject.Find("Key4").GetComponent<KeyHolder>();
+        refIsSpawn8 = GameObject.Find("Key8").GetComponent<KeyHolder>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (refIsSpawn4.isSpawnP1 == false)
+        {
+            animator.SetBool("isHurtP2", true);
+        }
+        else if (refIsSpawn4.isSpawnP1 == true)
+        {
+            animator.SetBool("isHurtP2", false);
+        }
+
+        if (refIsSpawn8.isSpawnP2 == false)
+        {
+            animator.SetBool("isHurtP1", true);
+        }
+        else if (refIsSpawn8.isSpawnP2 == true)
+        {
+            animator.SetBool("isHurtP1", false);
+        }
+    }
+}

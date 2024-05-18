81dd0080dbf9ad7edd9d95f663cc25ce7e424d45
aslcolon <155334067+aslcolon@users.noreply.github.com>
2024-05-18 12:28:03 -0700

    Complete health bar logic

--- Pixel Game Jam 2024/Assets/Scripts/HealthBar.cs
+++ Pixel Game Jam 2024/Assets/Scripts/HealthBar.cs
@@ -8,16 +8,41 @@ public class HealthBar : MonoBehaviour
 
     public Slider slider;
 
-    // Slider starts at maximum health
-    public void SetMaxHealth(int health)
+    public KeyHolder refIsSpawn4, refIsSpawn8;
+
+    // Implement spawn timer
+    public double decRate = 0.1;
+    private float timer = 0;
+
+    private void Start()
     {
-        slider.maxValue = health;
-        slider.value = health;
+        refIsSpawn4 = GameObject.Find("Key4").GetComponent<KeyHolder>();
+        refIsSpawn8 = GameObject.Find("Key8").GetComponent<KeyHolder>();
     }
-   
-    public void setHealth(int health)
+
+    private void Update()
     {
-        slider.value = health;
+        if (timer < decRate)
+        {
+            timer += Time.deltaTime;
+        }
+        // Else if sequence is not yet spawned, spawn key and reset timer
+        else if (refIsSpawn4.isSpawnP1 == false && gameObject.name == "Health bar P1")
+        {
+            if (slider.value >= 4)
+            {
+                slider.value -= 4;
+            }
+            
+            timer = 0;
+        }
+        else if (refIsSpawn8.isSpawnP2 == false && gameObject.name == "Health bar P2")
+        {
+            if (slider.value >= 4)
+            {
+                slider.value -= 4;
+            }
+            timer = 0;
+        }
     }
-    
 }

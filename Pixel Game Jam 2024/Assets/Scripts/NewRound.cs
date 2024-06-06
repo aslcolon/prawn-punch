9b2bb40d233a65f7d15b13a426c0a029d82e1cd5
aslcolon <155334067+aslcolon@users.noreply.github.com>
2024-06-05 17:02:53 -0700

    Clean up and comment code

--- Pixel Game Jam 2024/Assets/Scripts/NewRound.cs
+++ Pixel Game Jam 2024/Assets/Scripts/NewRound.cs
@@ -19,16 +19,16 @@ public class NewRound : MonoBehaviour
 
     private bool roundComplete = false;
 
-
     public int roundCounter = 0;
-
 
     // Start is called before the first frame update
     void Start()
     {
+        // Initialize refHealth as HealthBar componenets attached to each health bar
         refHealthP1 = GameObject.Find("Health bar P1").GetComponent<HealthBar>();
         refHealthP2 = GameObject.Find("Health bar P2").GetComponent<HealthBar>();
 
+        // Call functions to start first round
         nextRound();
         Invoke(nameof(currentRound), 2);
     }
@@ -36,51 +36,59 @@ public class NewRound : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If at least one player has zero health
         if ((refHealthP1.slider.value == 0 && refHealthP2.slider.value > 0) || (refHealthP2.slider.value == 0 && refHealthP1.slider.value > 0))
         {
-            roundComplete = false;
+            roundComplete = false; // Set roundComplete to false
 
-            for (int j = 1; j < 9; j++)
-            {
-                GameObject.Find("Prefab Spawn" + (j).ToString()).GetComponent<SpriteRenderer>().enabled = false;
-            }
+            // Disable key inputs while round popup is displayed
             for (int i = 1; i < 9; i++)
             {
+                GameObject.Find("Prefab Spawn" + (i).ToString()).GetComponent<SpriteRenderer>().enabled = false;
                 GameObject.Find("Prefab Spawn" + (i).ToString()).GetComponent<KeyInput>().enabled = false;
             }
 
+            // Call functions to display round popups
             Invoke(nameof(nextRound), 2);
             Invoke(nameof(currentRound), 4);
         }
 
+        // If round 3 is reached, disable round popup loop script
         if (roundCounter == 3)
         {
             GameObject.Find("UI").GetComponent<NewRound>().enabled = false;
         }
     }
 
+    // Awake function to implement audio
     private void Awake()
     {
         audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
     }
 
+    // nextRound function to declare round number and display popup at beginning of round
     private void nextRound()
     {
+        // If round is not complete
         if (roundComplete == false)
         {
-            roundCounter++;
+            roundCounter++; // Increase round count
+            // Reset player health
             refHealthP1.slider.value = 100;
             refHealthP2.slider.value = 100;
+
+            // Spawn popup to declare round number
             var roundPopup = Instantiate(roundPopupPrefab, transform.position, transform.rotation);
-            
 
             for (int i = 0; i < 3; i++)
             {
-
+                // For corresponding round
                 if (roundCounter == i + 1)
                 {
+                    // Popup sprite dependent on round count number
                     GameObject.Find("Round").GetComponent<SpriteRenderer>().sprite = roDisplay[i];
 
+                    // Play audio to corresponding round count number
                     if (i == 0)
                     {
                         audioManager.PlaySFX(audioManager.bellSignal1);
@@ -94,26 +102,24 @@ public class NewRound : MonoBehaviour
                         audioManager.PlaySFX(audioManager.bellSignal3);
                     }
                 }
-
-                //if (refHealthP1.oppPoint == numOfSeq * i || refHealthP2.oppPoint == numOfSeq * i)
             }
-            roundComplete = true;
+            roundComplete = true; // Round resets
         }
-
     }
 
+    // currentRound function 
     private void currentRound()
     {
+        // If round is complete
         if (roundComplete == true)
         {
             Debug.Log("STILL A NEW ROUND");
-            Destroy(GameObject.Find("Round Popup(Clone)"));
-            for (int j = 1; j < 9; j++)
-            {
-                GameObject.Find("Prefab Spawn" + (j).ToString()).GetComponent<SpriteRenderer>().enabled = true;
-            }
+            Destroy(GameObject.Find("Round Popup(Clone)")); // Destroy round popup
+
+            // Enable key inputs
             for (int i = 1; i < 9; i++)
             {
+                GameObject.Find("Prefab Spawn" + (i).ToString()).GetComponent<SpriteRenderer>().enabled = true;
                 GameObject.Find("Prefab Spawn" + (i).ToString()).GetComponent<KeyInput>().enabled = true;
             }
         }
--- Pixel Game Jam 2024/Assets/Scripts/PlayerWin.cs
+++ Pixel Game Jam 2024/Assets/Scripts/PlayerWin.cs
@@ -22,36 +22,38 @@ public class PlayerWin : MonoBehaviour
 
     private bool pointsCounted3 = false;
 
-
     // Start is called before the first frame update
     void Start()
     {
+        // Initialize refHealth as HealthBar componenets attached to each health bar
         refHealthP1 = GameObject.Find("Health bar P1").GetComponent<HealthBar>();
         refHealthP2 = GameObject.Find("Health bar P2").GetComponent<HealthBar>();
 
+        // Initialize refIsSpawn as KeyHolder components attached to each key holder
         refIsSpawn4 = GameObject.Find("Key4").GetComponent<KeyHolder>();
         refIsSpawn8 = GameObject.Find("Key8").GetComponent<KeyHolder>();
 
+        // Initialize refRoundCounter
         refRoundCounter = GameObject.Find("UI").GetComponent<NewRound>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // If at least one player has zero health
         if ((refHealthP1.slider.value == 0 && refHealthP2.slider.value > 0) || (refHealthP2.slider.value == 0 && refHealthP1.slider.value > 0))
         {
-
+            // For corresponding round
             for (int i = 1; i < 4; i++)
             {
                 if (refRoundCounter.roundCounter == i)
                 {
-
-                   
-
+                    // If player 2 hit last, they earn a star
                     if (refIsSpawn4.isSpawn == false)
                     {
                         GameObject.Find("Star" + i + " P1").GetComponent<SpriteRenderer>().enabled = true;
                     }
+                    // Else if player 1 hit last, they earn a star
                     else if (refIsSpawn8.isSpawn == false)
                     {
                         GameObject.Find("Star" + i + " P2").GetComponent<SpriteRenderer>().enabled = true;
@@ -72,8 +74,10 @@ public class PlayerWin : MonoBehaviour
                 pointsCounted1 = true;
             }*/
 
+            // If round 3
             if (refRoundCounter.roundCounter == 3 && pointsCounted3 == false)
             {
+                // Count number of stars for each player
                 for (int i = 1; i < 4; i++)
                 {
                     if (GameObject.Find("Star" + i + " P1").GetComponent<SpriteRenderer>().enabled == true)
@@ -85,8 +89,10 @@ public class PlayerWin : MonoBehaviour
                         pointsP2++;
                     }
                 }
+
                 pointsCounted3 = true;
 
+                // Winner is determined on greater number of points
                 if (pointsP1 > pointsP2)
                 {
                     winner = 1;
@@ -96,10 +102,9 @@ public class PlayerWin : MonoBehaviour
                     winner = 2;
                 }
             }
-
         }
 
-
+        // Win menu popup depending on winner
         if (GameObject.Find("UI").GetComponent<NewRound>().enabled == false && (winner == 1 || winner == 2))
         {
             GameObject.Find("Win Menu").GetComponent<Image>().enabled = true;
